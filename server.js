5e4b642880bd06108aa7ad62806261484687e9d9
billygotclout <williamsoluwademilade@gmail.com>
2024-10-18 16:46:57 +0100

    cors issue trial fix

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ connectDb();
 
 app.use(express.json());
 app.use(cors());
+app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 const logsDir = path.join(__dirname, "logs");
 if (!fs.existsSync(logsDir)) {
   fs.mkdirSync(logsDir);
