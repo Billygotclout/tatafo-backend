c05f009f81fe7e11c2dbfe5a2b5ee3ac9b7395f9
billygotclout <williamsoluwademilade@gmail.com>
2024-10-18 16:39:22 +0100

    logs issue vercel deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const logger = require("./utils/logger");
 const connectDb = require("./config/dbconnection");
+const fs = require("fs");
 require("dotenv").config();
 const port = process.env.PORT || 3001;
 const app = express();
@@ -10,7 +11,10 @@ connectDb();
 
 app.use(express.json());
 app.use(cors());
-
+const logsDir = path.join(__dirname, "logs");
+if (!fs.existsSync(logsDir)) {
+  fs.mkdirSync(logsDir);
+}
 app.get("/", (req, res) => {
   res.send("API is running");
 });
