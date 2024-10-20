e5ab59d1c9b1e891e8ee788f189d4e6208c72d50
billygotclout <williamsoluwademilade@gmail.com>
2024-10-20 07:41:20 +0100

    password reset changes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -62,7 +62,7 @@ const forgotPassword = async (req, res, next) => {
       text:
         `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n` +
         `Please click on the following link, or paste this into your browser to complete the process:\n\n` +
-        `http://localhost:5173/reset-password/${forgotPasswordToken}\n\n` +
+        `http://localhost:5173/reset-password/&token=${forgotPasswordToken}\n\n` +
         `If you did not request this, please ignore this email and your password will remain unchanged.\n`,
     });
 
